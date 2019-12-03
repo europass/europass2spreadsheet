6fd40276c48598620473a43f93ef7c307d0c8dd2
john-stam <js@eworx.gr>
2019-12-03 13:14:23 +0200

    #EPAS-2003 File constructor not supported in IE 11 and Edge fix

--- public/scripts.js
+++ public/scripts.js
@@ -85,10 +85,7 @@ $(document).ready(function () {
                 }
 
                 var type = request.getResponseHeader("Content-Type");
-                var blob =
-                    typeof File === "function"
-                        ? new File([this.response], filename, {type: type})
-                        : new Blob([this.response], {type: type});
+                var blob = new Blob([this.response], {type: type});
 
                 if (typeof window.navigator.msSaveBlob !== "undefined") {
                     // IE workaround for "HTML7007: One or more blob URLs were revoked by closing the blob for which they were created. These URLs will no longer resolve as the data backing the URL has been freed."
